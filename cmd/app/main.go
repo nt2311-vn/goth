dbea19b39f31d47490cf787018fcb70e18b6d931
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-04 17:53:28 +0700

    serve address

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	router.Get("/foo", handleFoo)
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP server started", "listenAddr", listenAddr)
+	http.ListenAndServe(listenAddr, router)
+}
+
+func handleFoo(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("foo"))
 }
